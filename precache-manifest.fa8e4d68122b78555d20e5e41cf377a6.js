f56e4e1ace1c0ade770bd655e34191c48e3870e5
theseanco <theseancotterill@live.com>
2020-01-23 16:58:51 +0000

    Updates

---
+++ precache-manifest.fa8e4d68122b78555d20e5e41cf377a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64537be2bbad0cff22ce",
+    "url": "/infinidog/static/css/main.60f20783.chunk.css"
+  },
+  {
+    "revision": "64537be2bbad0cff22ce",
+    "url": "/infinidog/static/js/main.64537be2.chunk.js"
+  },
+  {
+    "revision": "3b5f9931b266c6d6ab2a",
+    "url": "/infinidog/static/js/1.3b5f9931.chunk.js"
+  },
+  {
+    "revision": "5970e1e8874df0d158e4",
+    "url": "/infinidog/static/js/runtime~main.5970e1e8.js"
+  },
+  {
+    "revision": "6ba3928c5d9c9941ca045fb239fdd4fd",
+    "url": "/infinidog/index.html"
+  }
+];
